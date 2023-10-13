c3d30dde4f9899310d5d53e5a003178ad76b93c7
LeonKohli <98176333+LeonKohli@users.noreply.github.com>
2023-10-13 12:13:08 +0200

    frontent with vue

---
+++ frontend/src/components/TestComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="bg-green-500 text-white p-4 rounded">
+      {{ message }}
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: "TestComponent",
+    data() {
+        return {
+            message: 'Fetching...'
+        };
+    },
+    created() {
+        axios.get('http://localhost:5000/api/test')
+            .then(response => {
+                this.message = response.data.message;
+            })
+            .catch(error => {
+                this.message = `Error fetching data: ${error.message}`;
+            });
+    }
+};
+</script>
